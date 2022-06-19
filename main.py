5828a209cb53dc6a1339e25d265009772b3e2dac
porudev <porudev@gmail.com>
2022-06-19 10:25:49 +0700

    added main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import math
+
+total_match = input("Total Match: ").replace('%', '')
+current_winrate = input("Current winrate: ").replace('%', '')
+wanted_winrate = input("Wanted Winrate: ").replace('%', '')
+
+print(total_match)
+print(current_winrate)
+print(wanted_winrate)
