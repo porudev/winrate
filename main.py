efc9b0e2de93b6669cc2b053dafd096c79d6804c
porudev <porudev@gmail.com>
2022-06-20 17:36:22 +0700

    moved to functional

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import math
 
 print('┌──────────────────────── Input ──────────────────────────┐')
-total_match = input("├ Total Match:          ").replace('%', '')
-current_winrate = input("├ Current winrate:      ").replace('%', '')
-wanted_winrate = input("├ Wanted Winrate:       ").replace('%', '')
 
-total_match = float(total_match)
-current_winrate = float(current_winrate)
-wanted_winrate = float(wanted_winrate)
+def get_input() -> float:
+    total_match = input("├ Total Match:          ").replace('%', '')
+    current_winrate = input("├ Current winrate:      ").replace('%', '')
+    wanted_winrate = input("├ Wanted Winrate:       ").replace('%', '')
 
-current_win = (total_match*current_winrate)/100
-needed_match = ((100*current_win) - (wanted_winrate*total_match))/(wanted_winrate-100)
-estimate_win = needed_match+(needed_match*(current_winrate/100))
+    total_match = float(total_match)
+    current_winrate = float(current_winrate)
+    wanted_winrate = float(wanted_winrate)
 
+    if not total_match < 0.0 or current_winrate < 0.0 or wanted_winrate < 0.0:
+        return total_match, current_winrate, wanted_winrate
+
+    print("Value can't be lower than 0, canceling")
+
+def calculate(total_match, wanted_winrate, current_winrate) -> float:
+    current_win = (total_match*current_winrate)/100
+    needed_match = ((100*current_win) - (wanted_winrate*total_match))/(wanted_winrate-100)
+    estimate_win = needed_match+(needed_match*(current_winrate/100))
+
+def print_value()
 print('├────────────────────── Summerize ────────────────────────┤')
 print(f'├ Total Match:          {int(total_match):,}')
 print(f'├ Current Winrate:      {current_winrate}%')
